1fab2e631d0683f06c24119c405ffd4bb19ceda9
zawzawzaw <zawzawzaw@gmail.com>
2016-02-15 18:04:15 +0800

    updated

--- js/main.js
+++ js/main.js
@@ -30,12 +30,16 @@ $(document).ready(function(){
 
     var scrolled = false;
     var firstScroll = true;
-    var mainContentOffset = $('#main-content').offset().top;
+    var mainContentOffset = $('#main-content').offset().top - 63;
+
+    $(window).on('resize', function(){
+        mainContentOffset = $('#main-content').offset().top - 63; // change if window resize
+    });
 
     function scrollToContent() {
-        var scrollPos = $(window).scrollTop();        
+        var scrollPos = $(window).scrollTop();
 
-        if(scrollPos > 0 && scrollPos <= mainContentOffset) {
+        if(scrollPos >= 0 && scrollPos <= mainContentOffset) {
 
             if(firstScroll==true) {
                 firstScroll = false;
@@ -46,7 +50,7 @@ $(document).ready(function(){
                     handleScrollbar: false
                 });
 
-                var target_y = $('#content-wrapper').offset().top + 5;
+                var target_y = $('#content-wrapper').offset().top - 63; // minus out the height of menu 
 
                 TweenMax.to($(window), 1.2, {
                     scrollTo: { y:target_y, autoKill: false }, 
@@ -63,9 +67,7 @@ $(document).ready(function(){
     }
 
     function showHideArrow()  {
-        var scrollPos = $(window).scrollTop();   
-
-        // console.log(scrollPos);
+        var scrollPos = $(window).scrollTop();
     
         if( ( scrollPos != 0 ) ) {            
             $('.scroll-to-content .arrow-big').addClass('hide');
@@ -81,9 +83,9 @@ $(document).ready(function(){
     }
 
     function addBorder() {
-        var scrollPos = $(window).scrollTop();       
+        var scrollPos = $(window).scrollTop();            
 
-         if(scrollPos >= mainContentOffset){
+         if(scrollPos + 3 >= mainContentOffset){
             $('#menu-logo-wrapper').addClass('white-version');
             $('#mobile-menu-logo-wrapper').addClass('white-version');
         }else {
@@ -95,15 +97,16 @@ $(document).ready(function(){
     $(window).on('scroll', function() {        
         showHideArrow();
         addBorder();                       
-        scrollToContent();
+        // scrollToContent();
     });
 
     showHideArrow();
     
     $(window).on('mousewheel', function(event) {
         // console.log(event.deltaX, event.deltaY, event.deltaFactor);
-        if(event.deltaY<0) {
-            // scrollToContent();
+        if(event.deltaY<=0) {
+            // console.log(event.deltaY)
+            scrollToContent();
         }
     });
 
@@ -116,22 +119,26 @@ $(document).ready(function(){
     $('.scroll-to-content').on('click', function(e){
         e.preventDefault();
         var currentId = $(this).attr('href');
-        var target_y = $(currentId).offset().top + 5;
+        var target_y = $(currentId).offset().top - 63;
 
         TweenMax.to($(window), 1.2, {scrollTo:{y:target_y,autoKill: false}, ease:Quad.easeInOut});
     });
 
     function scrollToThis(currentId){
-        console.log(currentId)
+        if(currentId=='#about')
+            var scrollTopPos = $(currentId).offset().top - 63;
+        else
+            var scrollTopPos = $(currentId).offset().top - 25;
+
         $('body').animate({
-            scrollTop: $(currentId).offset().top - 25
+            scrollTop: scrollTopPos
         }, 1200);
     }
 
     $('.main-nav li a').on('click', function(e){
         e.preventDefault();
-        $('.active-nav').removeClass('active-nav');
-        $(this).addClass('active-nav')
+        // $('.active-nav').removeClass('active-nav');
+        // $(this).addClass('active-nav')
         scrollToThis($(this).attr('href'));
     });
 
@@ -139,42 +146,10 @@ $(document).ready(function(){
     ///////
     ///////
 
-    $(".main-nav > li").children('a').on('click', function(e){
-        e.stopPropagation();        
-        if($(this).hasClass('has-child')) {
-            e.preventDefault();
-            $('.main-nav li a').not(this).removeClass('active');
-            $('.main-nav li').not($(this).parent()).removeClass('active');
-            $(this).toggleClass('active');
-            $(this).parent().toggleClass('active');
-
-            if($(this).hasClass('active')) {
-                $("#menu-logo-wrapper").removeClass('add-border');
-            }
-
-            $("#menu-logo-wrapper").toggleClass('add-border');
-            // $('.sidebar-nav').toggleClass('open').toggleClass('fadeInLeft');
-        }
-    });
-
-    $('html').click(function() {
-        $('.main-nav li a').removeClass('active');
-        $('.main-nav li').removeClass('active');
-        $("#menu-logo-wrapper").removeClass('add-border');
-    });   
-
-    // mobile menu
     $('.toggle-menu').on('click', function(e){
         e.preventDefault();     
         $('.main-nav').toggleClass('show-nav');
     });
-
-    // $('.sub-nav-toggle').on('click', function(e){
-    //     e.preventDefault();        
-    //     $(this).toggleClass('active');        
-    //     $(this).next('ul.mobile-nav-child').toggleClass('open');
-    //     $(this).parent('li').toggleClass('active');
-    // });    
 
     $(".carousel").swiperight(function() {  
         $(this).carousel('prev');  
@@ -183,6 +158,272 @@ $(document).ready(function(){
         $(this).carousel('next');  
     });
 
+    // twitter custom scrollbar
     $('.scrollbar-dynamic').scrollbar();
+
+    ///////
+
+    $('#instafeed').on('click', 'a.insta_small', function(e){
+        e.preventDefault();
+        $('#insta_big').attr('src', $(this).children('img').data('imgsrc'));
+    });    
+
+    ///////
+
+    var feed = new Instafeed({
+        get: 'tagged',
+        tagName: 'scooptheraphy',
+        clientId: '7ff984bc8161403aa84f7c52facc0aa6',
+        template: '<li><a href="javascript:void(0);" class="insta_small"><img data-imgsrc="{{image}}" src="{{image}}" /></a></li>',
+        resolution: 'standard_resolution',
+        limit: 5,
+        after: function(){
+            $('.insta_small').last().trigger('click');
+        }
+    });
+    feed.run();
+
+    ///////
+
+    var config1 = {
+      "id": '697988421285875713',
+      "domId": 'example1',
+      "maxTweets": 5,
+      "enableLinks": true,
+      "showPermalinks": false,
+      "showInteraction": false,
+      "showInteraction": false,
+      "customCallback": handleTweets
+    };
+
+    function handleTweets(tweets) {
+        var x = tweets.length;
+        var n = 0;
+        var element = document.getElementById('example1');
+        var html = '<ul class="test">';
+        while(n < x) {        
+          html += '<li>' + tweets[n] + '</li>';
+          n++;
+        }
+        html += '</ul>';
+        element.innerHTML = html;
+    }
+
+    twitterFetcher.fetch(config1);
+
+    /////////
+
+    var styles = [
+        {
+            "featureType": "water",
+            "elementType": "geometry.fill",
+            "stylers": [
+                {
+                    "color": "#d3d3d3"
+                }
+            ]
+        },
+        {
+            "featureType": "transit",
+            "stylers": [
+                {
+                    "color": "#808080"
+                },
+                {
+                    "visibility": "off"
+                }
+            ]
+        },
+        {
+            "featureType": "road.highway",
+            "elementType": "geometry.stroke",
+            "stylers": [
+                {
+                    "visibility": "on"
+                },
+                {
+                    "color": "#b3b3b3"
+                }
+            ]
+        },
+        {
+            "featureType": "road.highway",
+            "elementType": "geometry.fill",
+            "stylers": [
+                {
+                    "color": "#ffffff"
+                }
+            ]
+        },
+        {
+            "featureType": "road.local",
+            "elementType": "geometry.fill",
+            "stylers": [
+                {
+                    "visibility": "on"
+                },
+                {
+                    "color": "#ffffff"
+                },
+                {
+                    "weight": 1.8
+                }
+            ]
+        },
+        {
+            "featureType": "road.local",
+            "elementType": "geometry.stroke",
+            "stylers": [
+                {
+                    "color": "#d7d7d7"
+                }
+            ]
+        },
+        {
+            "featureType": "poi",
+            "elementType": "geometry.fill",
+            "stylers": [
+                {
+                    "visibility": "on"
+                },
+                {
+                    "color": "#ebebeb"
+                }
+            ]
+        },
+        {
+            "featureType": "administrative",
+            "elementType": "geometry",
+            "stylers": [
+                {
+                    "color": "#a7a7a7"
+                }
+            ]
+        },
+        {
+            "featureType": "road.arterial",
+            "elementType": "geometry.fill",
+            "stylers": [
+                {
+                    "color": "#ffffff"
+                }
+            ]
+        },
+        {
+            "featureType": "road.arterial",
+            "elementType": "geometry.fill",
+            "stylers": [
+                {
+                    "color": "#ffffff"
+                }
+            ]
+        },
+        {
+            "featureType": "landscape",
+            "elementType": "geometry.fill",
+            "stylers": [
+                {
+                    "visibility": "on"
+                },
+                {
+                    "color": "#efefef"
+                }
+            ]
+        },
+        {
+            "featureType": "road",
+            "elementType": "labels.text.fill",
+            "stylers": [
+                {
+                    "color": "#696969"
+                }
+            ]
+        },
+        {
+            "featureType": "administrative",
+            "elementType": "labels.text.fill",
+            "stylers": [
+                {
+                    "visibility": "on"
+                },
+                {
+                    "color": "#737373"
+                }
+            ]
+        },
+        {
+            "featureType": "poi",
+            "elementType": "labels.icon",
+            "stylers": [
+                {
+                    "visibility": "off"
+                }
+            ]
+        },
+        {
+            "featureType": "poi",
+            "elementType": "labels",
+            "stylers": [
+                {
+                    "visibility": "off"
+                }
+            ]
+        },
+        {
+            "featureType": "road.arterial",
+            "elementType": "geometry.stroke",
+            "stylers": [
+                {
+                    "color": "#d6d6d6"
+                }
+            ]
+        },
+        {
+            "featureType": "road",
+            "elementType": "labels.icon",
+            "stylers": [
+                {
+                    "visibility": "off"
+                }
+            ]
+        },
+        {},
+        {
+            "featureType": "poi",
+            "elementType": "geometry.fill",
+            "stylers": [
+                {
+                    "color": "#dadada"
+                }
+            ]
+        }
+    ];
+
+    function setMarkerLatLng(lat, lng) {
+        return new google.maps.LatLng(lat, lng);
+    }
+
+    var ScoopLatlng = setMarkerLatLng(1.3239576,103.8506361);
+    var mapOptions = {
+      mapTypeControlOptions: {  
+          mapTypeIds: ['Styled']
+      },  
+      zoom: 15,
+      center: ScoopLatlng,
+      disableDefaultUI: true,   
+      mapTypeId: 'Styled'
+    };
+
+    map = new google.maps.Map(document.getElementById('map'),
+      mapOptions);
+    var styledMapType = new google.maps.StyledMapType(styles, { name: 'Styled' });  
+    map.mapTypes.set('Styled', styledMapType);
+
+    var marker = new google.maps.Marker({
+        position: ScoopLatlng,
+        map: map,
+        title: 'Scoop Therapy!',
+        icon: 'images/icons/map-pin.png'
+    });
     
 });
